c5003c76a3cfc63cb9c472db95e0fb6af17b4fb1
muhammadidzoha <muhammadidzoha@gmail.com>
2026-07-24 01:08:23 +0700

    [UPDATE] Finished Model WifiNetwork

--- apps/SmartGreenhouse/src/main/java/com/nxp/example/smartgreenhouse/model/wifi/WifiNetwork.java
+++ apps/SmartGreenhouse/src/main/java/com/nxp/example/smartgreenhouse/model/wifi/WifiNetwork.java
@@ -7,4 +7,26 @@ public class WifiNetwork {
     private final boolean secured;
     private final boolean connected;
 
+    public WifiNetwork(String name, int rssi, boolean secured, boolean connected) {
+        this.name = name;
+        this.rssi = rssi;
+        this.secured = secured;
+        this.connected = connected;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getRssi() {
+        return rssi;
+    }
+
+    public boolean isSecured() {
+        return secured;
+    }
+
+    public boolean isConnected() {
+        return connected;
+    }
 }
